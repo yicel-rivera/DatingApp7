32d5932b3fcbab8718602d300fb826939b07807b
yicel-rivera <122029698+yicel-rivera@users.noreply.github.com>
2023-03-21 14:01:26 -0400

    Second-half and end of Section 10

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs';
@@ -12,23 +12,28 @@ import { MembersService } from 'src/app/_services/members.service';
   templateUrl: './member-edit.component.html',
   styleUrls: ['./member-edit.component.css']
 })
-export class MemberEditComponent implements OnInit{
+export class MemberEditComponent implements OnInit {
   @ViewChild('editForm') editForm: NgForm | undefined;
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any) {
+    if (this.editForm?.dirty) {
+      $event.returnValue = true;
+    }
+  }
   member: Member | undefined;
   user: User | null = null;
 
-  constructor(private accountService: AccountService, private memberService: MembersService, 
-      private toastr: ToastrService) {
+  constructor(private accountService: AccountService, private memberService: MembersService,
+    private toastr: ToastrService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe({
       next: user => this.user = user
     })
-   }
+  }
 
   ngOnInit(): void {
     this.loadMember();
   }
 
-  loadMember(){
+  loadMember() {
     if (!this.user) return;
     this.memberService.getMember(this.user.username).subscribe({
       next: member => this.member = member
@@ -36,9 +41,12 @@ export class MemberEditComponent implements OnInit{
   }
 
   updateMember() {
-    console.log(this.member);
-    this.toastr.success("Profile updated successfully")
-    this.editForm?.reset(this.member);
+    this.memberService.updateMember(this.editForm?.value).subscribe({
+      next: _ => {
+        this.toastr.success("Profile updated successfully")
+        this.editForm?.reset(this.member);
+      }
+    })
   }
 
 }
